bed51f12671a5541e9cf5fae599dc516e44e33e8
NaveenWebDev <ns222280@gmail.com>
2023-07-13 15:26:30 +0530

    remove jquery and add js

--- js/index.js
+++ js/index.js
@@ -1,88 +1,13 @@
-$(document).ready(function(){
-
-  $("#flip").click(function(){
-    $("#panel").slideToggle(300);
-    $("#arrow").toggleClass("rotate");
-
-    $("#panelOne").slideUp(300);
-    $("#arrowOne").removeClass("rotate");
-
-    $("#panelTwo").slideUp(300);
-    $("#arrowTwo").removeClass("rotate");
-
-    $("#panelThree").slideUp(300);
-    $("#arrowThree").removeClass("rotate");
-
-    $("#panelFour").slideUp(300);
-    $("#arrowFour").toggleClass("rotate");
-
-  });
-  $("#flipTwo").click(function(){
-
-    $("#panel").slideUp(300);
-    $("#arrow").removeClass("rotate");
-    
-    $("#panelOne").slideToggle(300);
-    $("#arrowOne").toggleClass("rotate");
-
-    $("#panelTwo").slideUp(300);
-    $("#arrowTwo").removeClass("rotate");
-
-    $("#panelThree").slideUp(300);
-    $("#arrowThree").removeClass("rotate");
-
-    $("#panelFour").slideUp(300);
-    $("#arrowFour").toggleClass("rotate");
-
-  });
-  $("#flipThree").click(function(){
-    $("#panelTwo").slideToggle(300);
-    $("#arrowTwo").toggleClass("rotate");
-
-    $("#panel").slideUp(300);
-    $("#arrow").removeClass("rotate");
-    
-    $("#panelOne").slideUp(300);
-    $("#arrowOne").removeClass("rotate");
-
-
-    $("#panelThree").slideUp(300);
-    $("#arrowThree").removeClass("rotate");
-
-    $("#panelFour").slideUp(300);
-    $("#arrowFour").toggleClass("rotate");
-  });
-  $("#flipFour").click(function(){
-    $("#panelThree").slideToggle(300);
-    $("#arrowThree").toggleClass("rotate");
-
-    $("#panelFour").slideUp(300);
-    $("#arrowFour").toggleClass("rotate");
-
-    $("#panelTwo").slideUp(300);
-    $("#arrowTwo").removeClass("rotate");
-
-    $("#panel").slideUp(300);
-    $("#arrow").removeClass("rotate");
-    
-    $("#panelOne").slideUp(300);
-    $("#arrowOne").removeClass("rotate");
-  });
-  $("#flipFive").click(function(){
-    $("#panelFour").slideToggle(300);
-    $("#arrowFour").toggleClass("rotate");
-
-    $("#panelThree").slideUp(300);
-    $("#arrowThree").removeClass("rotate");
-
-    $("#panelTwo").slideUp(300);
-    $("#arrowTwo").removeClass("rotate");
-
-    $("#panel").slideUp(300);
-    $("#arrow").removeClass("rotate");
-    
-    $("#panelOne").slideUp(300);
-    $("#arrowOne").removeClass("rotate");
-  });
-
-});
+const slider = document.querySelectorAll(".slider");
+const panel = document.querySelectorAll(".panel");
+  
+for (let index = 0; index < slider.length; index++) {
+slider[index].addEventListener("click", ()=>{
+  if(panel[index].style.display == "none"){
+    panel[index].style.display="block"
+  }else{
+    panel[index].style.display="none"
+  }
+})
+  
+}
